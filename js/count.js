3b9f7f1f0b3ff262cea1896fd0139521934fddde
Albert Attila <albattis@freemail.hu>
2021-07-18 01:36:04 +0200

    data count

--- js/count.js
+++ js/count.js
@@ -1,13 +1,16 @@
 "use strict"
 
-let roomHeight,oldalszamlalo,ajtoszamlalo,elsooldalfal,masodikoldalfal,harmadikoldalfal,negyedikoldalfal,elsoajtomagassag,elsoajtoszelesseg,masodikajtomagassag,masodikajtoszelesseg,harmadikajtomagassag,harmadikajtoszelesseg,negyedikajtomagassag,negyedikajtoszelesseg,otodikajtomagassag,otodikajtoszelesseg,hatodikajtomagassag,hatodikajtoszelesseg,hetedikajtomagassag,hetedikajtoszelesseg;
-
+let roomHeight,oldalszamlalo,ajtoszamlalo,ablakszamlalo,elsooldalfal,masodikoldalfal,harmadikoldalfal,negyedikoldalfal,elsoajtomagassag,elsoajtoszelesseg,masodikajtomagassag,masodikajtoszelesseg,harmadikajtomagassag,harmadikajtoszelesseg,negyedikajtomagassag,negyedikajtoszelesseg,otodikajtomagassag,otodikajtoszelesseg,hatodikajtomagassag,hatodikajtoszelesseg,hetedikajtomagassag,hetedikajtoszelesseg,elsoablakmagssag,elsoablakszelesseg,elsoablakatmero,masodikablakmagssag,masodikablakszelesseg,masodikablakatmero,harmadikablakmagssag,harmadikablakszelesseg,harmadikablakatmero,negyedikablakmagssag,negyedikablakszelesseg,negyedikablakatmero,otodikablakmagssag,otodikablakszelesseg,otodikablakatmero,hatodikablakmagssag,hatodikablakszelesseg,hatodikablakatmero,hetedikablakmagssag,hetedikablakszelesseg,hetedikablakatmero;
+//Ha már addig eljutottunk hogy megvannak az adatok akkor rogton if ágon belül ki kell számolni az adat cm2 részét is
+//Meg kell irni a kör és a tégla területszámitásást. Illetve ellenörzni hogy nem e négyzet a sztori. ha a oldal
+// b oldal egyenlö akkor négyzet!!!
 
 function osszegzes()
 {
     roomHeight=document.getElementById('roomHeight').value;
     szobaoldal();
     ajtok();
+    ablakok();
 }
 
 function szobaoldal()
@@ -72,5 +75,55 @@ if(ajtoszamlalo>=1)
         hetedikajtoszelesseg=document.getElementById('seventhdoorwidth').value;
     }
 console.log(elsoajtomagassag,elsoajtoszelesseg,masodikajtomagassag,masodikajtoszelesseg,harmadikajtomagassag,harmadikajtoszelesseg,negyedikajtomagassag,negyedikajtoszelesseg,otodikajtomagassag,otodikajtoszelesseg,hatodikajtomagassag,hatodikajtoszelesseg,hetedikajtomagassag,hetedikajtoszelesseg);
+
+}
+
+function ablakok()
+{
+ablakszamlalo=document.getElementById('windowSelect').value;
+
+if(ablakszamlalo>=1)
+{
+    elsoablakmagssag=document.getElementById('firstwindowheight').value;
+    elsoablakszelesseg=document.getElementById('firstwindowwidth').value;
+    elsoablakatmero=document.getElementById('firstround').value;
+}
+    if(ablakszamlalo>=2)
+    {
+        masodikablakmagssag=document.getElementById('secondwindowheight').value;
+        masodikablakszelesseg=document.getElementById('secondwindowwidth').value;
+        masodikablakatmero=document.getElementById('secondround').value;
+    }
+    if(ablakszamlalo>=3)
+    {
+        harmadikablakmagssag=document.getElementById('thirdwindowheight').value;
+        harmadikablakszelesseg=document.getElementById('thirdwindowwidth').value;
+        harmadikablakatmero=document.getElementById('thirdround').value;
+    }
+    if(ablakszamlalo>=4)
+    {
+       negyedikablakmagssag=document.getElementById('fourthwindowheight').value;
+        negyedikablakszelesseg=document.getElementById('fourthwindowwidth').value;
+        negyedikablakatmero=document.getElementById('fourthround').value;
+    }
+    if(ablakszamlalo>=5)
+    {
+        otodikablakmagssag=document.getElementById('fivethwindowheight').value;
+        otodikablakszelesseg=document.getElementById('fivethwindowwidth').value;
+        otodikablakatmero=document.getElementById('fivethround').value;
+    }
+    if(ablakszamlalo>=6)
+    {
+        hatodikablakmagssag=document.getElementById('sixthwindowheight').value;
+        hatodikablakszelesseg=document.getElementById('sixthwindowwidth').value;
+        hatodikablakatmero=document.getElementById('sixthround').value;
+    }
+    if(ablakszamlalo>=7)
+    {
+        hetedikablakmagssag=document.getElementById('seventhwindowheight').value;
+        hetedikablakszelesseg=document.getElementById('seventhwindowwidth').value;
+        hetedikablakatmero=document.getElementById('seventhround').value;
+    }
+
 
 }
